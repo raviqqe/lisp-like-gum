96173e2da5b704a48b38dde2fee55d61502a51cd
Yota Toyama <raviqqe@gmail.com>
2016-09-21 01:52:26 +0900

    Implement weight

--- src/weight.rs
+++ src/weight.rs
@@ -7,21 +7,29 @@ pub struct Weight {
 }
 
 impl Weight {
-  fn new() {
-
+  fn new(v: u64) {
+    Weight { value: v }
   }
-
 }
 
-
 impl Default for Weight {
-
+  fn default() -> Weight {
+    Weight { value: 2^16 }
+  }
 }
 
 impl Add for Weight {
+  type Output = Self;
 
+  fn add(self, rhs: Self) -> Self {
+    Weight { value: self.value + rhs.value }
+  }
 }
 
 impl Sub for Weight {
+  type Output = Self;
 
+  fn sub(self, rhs: Self) -> Self {
+    Weight { value: self.value - rhs.value }
+  }
 }
