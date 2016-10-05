1a2b2153b822a8bbdbc9492a9de934da0b29b700
Yota Toyama <raviqqe@gmail.com>
2016-10-06 01:13:41 +0900

    Receive Refs by references from Object trait

--- farmem/src/object.rs
+++ farmem/src/object.rs
@@ -6,6 +6,6 @@ use reference::Ref;
 
 
 pub trait Object: Serialize + Deserialize {
-  fn refs(&self) -> Vec<Ref>;
-  fn nearby_refs(&self) -> Vec<Ref>;
+  fn refs(&self) -> Vec<&Ref>;
+  fn nearby_refs(&self) -> Vec<&Ref>;
 }
