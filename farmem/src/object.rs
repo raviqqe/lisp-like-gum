a78e8bba8cb94efd996fc291288b9835dd78c4a8
Yota Toyama <raviqqe@gmail.com>
2016-10-05 13:52:25 +0900

    Add Object trait methods

--- farmem/src/object.rs
+++ farmem/src/object.rs
@@ -1,6 +1,11 @@
 use serde::ser::Serialize;
 use serde::de::Deserialize;
 
+use reference::Ref;
 
 
-pub trait Object: Serialize + Deserialize {}
+
+pub trait Object: Serialize + Deserialize {
+  fn refs(&self) -> Vec<Ref>;
+  fn nearby_refs(&self) -> Vec<Ref>;
+}
